e70ac54ca909721473b3a7a4c91a2d923718923d
meghanaturner <meghanaturner1@gmail.com>
2022-08-08 13:40:49 -0700

    Functionalized saveas TIF, older version of read 3D tiff

--- add_imagej_metadata_to_tifs.m
+++ add_imagej_metadata_to_tifs.m
@@ -1,5 +1,7 @@
 function add_imagej_metadata_to_tifs(preproc_data_folder, experiment_prefix)
-
+%
+% function add_imagej_metadata_to_tifs(preproc_data_folder, experiment_prefix)
+%
 % DESCRIPTION
 % This function generates new versions of the PreProcessedData tiff stacks
 % which will have FIJI-specific metadata associated with them. This change 
@@ -19,53 +21,30 @@ function add_imagej_metadata_to_tifs(preproc_data_folder, experiment_prefix)
 %
 % Author (contact): Meghan Turner (meghanaturner1@gmail.com)
 % Created: 04/19/2022
-% Last Updated:
-%% 
+% Last Updated: 08/06/2022
+
+%%
+% Set defaults
+bit_depth = 16; %we take most of our data at 16 bit depth
 
 curr_dir = [preproc_data_folder, filesep, experiment_prefix];
 all_tifs = dir([curr_dir, filesep, '*_ch0*tif']);
 
+% Re-save all PreProcessed TIF files in folder
 wbar = waitbar(0, ['Adding ImageJ metadata to ' experiment_prefix ' PreProc tiffs']);
 for i = 1:length(all_tifs)
     waitbar(i/length(all_tifs), wbar);
 
-    curr_tif = tiffreadVolume([curr_dir, filesep, all_tifs(i).name]);
-    im_stack = curr_tif; 
+    tifffile_name = all_tifs(i).name;
+    % custom function for older MATLAB versions:
+    curr_tif = read_tiff_stack([curr_dir, filesep, tifffile_name]); 
+%     % Newer versions of MATLAB have a built-in function to read 3D tiffs:
+%     curr_tif = tiffreadVolume([curr_dir, filesep, tifffile_name]);
+    im_stack = curr_tif;
     
-    % Make a new Tiff object, name it the same as the old tif file
-    t = Tiff([curr_dir, filesep, all_tifs(i).name],'w');
+    % Save as a Tiff object with FIJI metadata included
+    saveas_imagej_tif_xyz(im_stack, curr_dir, tifffile_name, bit_depth)
     
-    fiji_descr = ['ImageJ=1.52u', newline, ...
-                  'images=', num2str(size(im_stack,3)), newline,... 
-                  'slices=', num2str(size(im_stack,3)), newline,...
-                  'loop=false', newline,...  
-                  'min=0.0', newline,...      
-                  'max=', num2str(max(im_stack(:)))];  % 16-bit image
-    %             'channels=' num2str(1) newline...
-    %             'frames=' num2str(1) newline... 
-    %             'hyperstack=true' newline...
-    %             'mode=grayscale' newline... 
-
-    % Add the ImageJ/FIJI metadats to the Tiff tag structure
-    tagstruct.ImageDescription = fiji_descr;
-    % 7 other mandatory tags per the Tiff class documentation:
-    tagstruct.ImageLength = size(im_stack,1);
-    tagstruct.ImageWidth = size(im_stack,2);
-    tagstruct.Photometric = Tiff.Photometric.MinIsBlack;
-    tagstruct.BitsPerSample = 16;
-    tagstruct.SamplesPerPixel = 1;
-    tagstruct.Compression = Tiff.Compression.LZW; % DO NOT use None - it circularly permutes the pixels in xy space
-    tagstruct.PlanarConfiguration = Tiff.PlanarConfiguration.Chunky;
-    % optional tag:
-    tagstruct.SampleFormat = Tiff.SampleFormat.UInt;
-    
-    % Now write the image stack to the new Tiff object
-    for slice = 1:size(im_stack,3)
-        t.setTag(tagstruct)
-        t.write(im2uint16(im_stack(:,:,slice)));
-        t.writeDirectory(); % saves a new page in the tiff file
-    end
-    t.close()
 end
 close(wbar)
 disp('Done!')
